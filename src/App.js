d68e67bb7bbe97621d07eafb032e946f44577eda
blaqbox-prime <karabosambo@outlook.com>
2021-06-09 17:07:12 +0200

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import TextEditor from './TextEditor';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+
+        <Route path="/documents/:id">
+          <TextEditor/>
+        </Route>
+        <Route exact path="/">
+          <Redirect to={`/documents/${uuidv4()}`}/>
+        </Route>
+
+      </Switch>
+      
+    
+    </Router>
   );
 }
 
---
+++ src/TextEditor.js
@@ -0,0 +1,113 @@
+import React, {useCallback, useEffect, useState} from 'react'
+import Quill from "quill";
+import 'quill/dist/quill.snow.css';
+import {io} from "socket.io-client";
+import {useParams} from 'react-router-dom';
+
+const TOOLBAR_OPTIONS = [
+    [{header: [1,2,3,4,5,6,false]}],
+    [{font:[]}],
+    [{ list: 'ordered'}, { list: 'bullet' }],
+    ["bold","italic","underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: 'sub'}, { script: 'super' }],
+    [{align: []}],
+    ["image","blockquote","code-block"],
+    ["clean"],
+]
+
+export default function TextEditor() {
+
+    const {id: docId} = useParams();
+    const [socket, setSocket] = useState();
+    const [quill, setQuill] = useState();
+
+    //create quill instance
+   const wrapperRef = useCallback((wrapper) => {
+       //check if wrapper exists
+       if(wrapper == null) return;
+       //cleanup
+       wrapper.innerHTML = '';
+        const editor = document.createElement('div');
+        wrapper.append(editor);
+       const q = new Quill(editor,{theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}});
+       q.disable();
+       q.setText("Loading Document....");
+       setQuill(q);
+       
+    }, [])
+
+    //Setup socket connection
+    useEffect(() => {
+        const s = io("http://localhost:3001");
+        setSocket(s);
+        //cleanup
+        return () => {
+            s.disconnect();
+        }
+    }, [])
+
+    //Send Quill changes
+    useEffect(() => {
+        if(socket == null || quill == null) return;
+        const handler = (delta, oldDelta, source) => {
+            if(source !== "user") return;
+            socket.emit('send-changes',delta);
+        }
+
+        quill.on('text-change', handler)
+        return () => {
+            quill.off('text-change', handler)
+        }
+    }, [socket,quill])
+
+    //Recieve Quill changes
+    useEffect(() => {
+        if(socket == null || quill == null) return;
+
+        const handler = (delta) => {
+            quill.updateContents(delta);
+        }
+
+        socket.on('recieve-changes', handler)
+        return () => {
+            socket.off('recieve-changes', handler)
+        }
+    }, [socket,quill])
+    
+    //Load Document
+   useEffect(() => {
+    if(socket ==  null || quill == null) return;
+
+    socket.once("load-document", doc => {
+        quill.setContents(doc);
+        quill.enable();
+    })
+
+    //send doc id to the server
+    socket.emit('get-document',docId);
+
+   },[socket, quill, docId]); 
+
+   const SAVE_INTERVAL_MS = 2000;
+
+   useEffect(()=>{
+    if(socket == null || quill == null) return;
+
+    const interval = setInterval(()=>{
+        socket.emit('save-document', quill.getContents());
+    },SAVE_INTERVAL_MS)
+
+    return () => {
+        clearInterval(interval);
+    }
+
+   },[socket,quill])
+
+    //Rendered Component
+    return (
+        <div className="container" ref={wrapperRef}>
+            Text Thing
+        </div>
+    )
+}
